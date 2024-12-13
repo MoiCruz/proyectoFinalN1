74493197f1527afe11c9ad8aa446c0ed83ce58dc
Moises Cruz <serviciopatrimonial@gmail.com>
2024-12-13 08:33:01 -0600

    primer commit viernes

--- script/script.js
+++ script/script.js
@@ -1,54 +1,93 @@
-//displays para capturar//
+// id de displays para capturar//
 const borrar = document.querySelector(`#borrar`)
 const hipoteca = document.querySelector(`#hipoteca`)
 const plazo = document.querySelector(`#plazo`)
 const tasaDeInteres = document.querySelector(`#tasaDeInteres`)
-//imputs radio//
-const tipoDeHipoteca = document.querySelector(`#tipoDeHipoteca`)
+//id de imputs radio//
+const tipoDeHipoteca = document.querySelector(`#reembolso`)
 const soloInteres = document.querySelector(`#soloInteres`)
-//ventana de resultados//
+//id de las ventanas de resultados//
 const imagen1 = document.querySelector(`#imagen1`)
 const calcular = document.querySelector(`#calcular`)
 const Resultados = document.querySelector(`#Resultados`)
-const pagosMensuales = document.querySelector(`#pagosMensuales`)
+const mensualidades = document.querySelector(`#mensualidades`)
 const pagoTotal = document.querySelector(`#pagoTotal`)
+// id de etiqueta <form> //
+const form = document.querySelector('#form');
 
-/*const form = document.querySelector('#form')
+// funcion para mostrar error por falta de valores//
+function mostrarError(e) {
+  e.preventDefault(); 
+  
+  const formData = new FormData(form);
+  let valor1 = formData.get('hipoteca');
+  let valor2 = formData.get('plazo');
+  let valor3 = formData.get('tasaDeInteres');
+  let valor4 = formData.get('reembolso');
+  let valor5 = formData.get('soloInteres');
+  
+  if (valor1 === "") {
+    form.elements.hipoteca.closest('label').classList.add('vacio');
+    form.querySelector('#dato1 + .errorCaptura').classList.remove('d-none'); 
+  } else {
+    form.elements.hipoteca.closest('label').classList.remove('vacio');
+    form.querySelector('#dato1 + .errorCaptura').classList.add('d-none');
+  }
 
-function calcular(e) {
-    e.preventDefault() 
-    const formData = new FormData(form)
-    let valor1 = formData.get('hipoteca'); 
-    let valor2 = formData.get('plazo'); 
-    let valor3 = formData.get('tazaDeInteres');
+  if (valor2 === "") {
+    form.elements.plazo.closest('label').classList.add('vacio');
+    form.querySelector('#dato2 + .errorCaptura').classList.remove('d-none'); 
+  } else {
+    form.elements.plazo.closest('label').classList.remove('vacio');
+    form.querySelector('#dato2 + .errorCaptura').classList.add('d-none');
+  }
 
-    
-    console.log(form.elements.hipoteca);
-    
-    errores(valor1, form.elements.hipoteca)
-    errores(valor2, form.elements.plazo)
-    errores(valor3, form.elements.tazaDeInteres)
+  if (valor3 === "") {
+    form.elements.tasaDeInteres.closest('label').classList.add('vacio');
+    form.querySelector('#dato3 + .errorCaptura').classList.remove('d-none'); 
+  } else {
+    form.elements.tasaDeInteres.closest('label').classList.remove('vacio');
+    form.querySelector('#dato3 + .errorCaptura').classList.add('d-none');
+  }
+  if (valor4 === "" && valor5 === "") {
+    form.querySelector('#errorInteres').classList.remove('d-none');
+  } else {
+    form.querySelector('#errorInteres').classList.add('d-none'); 
+  }
+ 
 }
+calcular.addEventListener('click', mostrarError);
 
-function errores(value, element) {
-    if (value === "") {
-        element.parentElement.classList.add('error')
-    } else {
-        element.parentElement.classList.remove('error')
+//funcion para ejecutar calculadora//
+ /*Nota: aprendi sobre la funcion de "Math.pow" su sintaxis es la siguiente "Math.pow(base, exponente)"*/
+     let mPrestamo = 0
+     let tasa = 0
+     let numeroDePagos = 0
+
+function calculadora(){
+    mPrestamo = parseInt(hipoteca.value)
+    tasa = parseFloat(tasaDeInteres.value)/100/12
+    numeroDePagos = parseInt(plazo.value )*12
+    if(mPrestamo !==0 && tasa !==0 && numeroDePagos !==0){
+        const numerador = mPrestamo * tasa * Math.pow(1 + tasa, numeroDePagos);
+        const denominador = Math.pow(1 + tasa, numeroDePagos) - 1;
+        const pagoMensual = numerador / denominador;
+        
+        const deudaTotal = pagoMensual * numeroDePagos
+ //"Intl.NumberFormat" habilita la separacion por comas en los numeros mayores a 3 cifras//
+        const formateador = new Intl.NumberFormat('en-US'); 
+
+        mensualidades.textContent = formateador.format(pagoMensual.toFixed(2));
+        pagoTotal.textContent = formateador.format(deudaTotal.toFixed(2));
     }
 }
-
-form.addEventListener('submit', calcular)*/
-
-calcular.addEventListener(`click`, respuesta)
-
-
+    hipoteca.addEventListener(`input`, calculadora) 
+    plazo.addEventListener(`input`, calculadora) 
+    tasaDeInteres.addEventListener(`input`, calculadora)   
 
 //funcion ventana de resultados//
 function respuesta(e){
     imagen1.classList.add(`d-none`)
-    Resultados.classList.remove(`d-none`)
-    pagosMensuales.textContent = 1000
-    pagoTotal.textContent = 10000
+    Resultados.classList.remove(`d-none`) 
 }
-
+     calcular.addEventListener(`click`, respuesta)
